b9d5539c3c1921776f548f09aa65d373ea8a3b82
Howard Feger <hfeger4@gmail.com>
2018-04-19 00:43:42 -0700

    add root directory, main app component, root reducer, store, and route util

---
+++ frontend/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+const App = () => (
+  <div>
+    <h1>Hello there</h1>
+  </div>
+);
+
+export default App;
