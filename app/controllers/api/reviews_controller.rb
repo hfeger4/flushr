6d6061aae3cf7e5741e442f990c4ac2f359f7d1e
Howard Feger <hfeger4@gmail.com>
2018-04-21 23:30:25 -0700

    add session reducer and api util

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,2 +1,18 @@
 class Api::ReviewsController < ApplicationController
+  def index
+    if params[:bathroom]
+      @reviews = Bathroom.find(params[:bathroom_id]).reviews
+      render :index
+    else
+      @reviews = Review.all
+    end
+  end
+  def create
+    @review = Review.new()
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:)
+  end
 end
