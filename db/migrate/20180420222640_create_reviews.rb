2b0198d4f0c13bb7fe84a1e1ca3690c385ce6312
Howard Feger <hfeger4@gmail.com>
2018-04-23 09:18:31 -0700

    add review and bathroom tables, add error actions

--- db/migrate/20180420222640_create_reviews.rb
+++ db/migrate/20180420222640_create_reviews.rb
@@ -1,6 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[5.1]
   def change
     create_table :reviews do |t|
+      t.text :body, null: false
+      t.integer :user_id, null: false
+      t.integer :bathroom_id, null: false
 
       t.timestamps
     end
--- db/migrate/20180420223943_create_bathrooms.rb
+++ db/migrate/20180420223943_create_bathrooms.rb
@@ -1,6 +1,10 @@
 class CreateBathrooms < ActiveRecord::Migration[5.1]
   def change
     create_table :bathrooms do |t|
+      t.string :location, null: false
+      t.string :description
+      t.string :image_url
+      t.integer :rating
 
       t.timestamps
     end
